c1dbabafa6d9108d70822180828df60d6551f85f
hoanganh05082002 <anh05082002@gmail.com>
2023-12-02 22:46:13 +0700

    hanh2122023

--- Models/DictionaryModel.Designer.cs
+++ Models/DictionaryModel.Designer.cs
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 ﻿// T4 code generation is enabled for model 'D:\GIT_Clone\Dictionary\Models\DictionaryModel.edmx'. 
+=======
+﻿// T4 code generation is enabled for model 'D:\GIT\Dictionary\Dictionary\Models\DictionaryModel.edmx'. 
+>>>>>>> Stashed changes
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
